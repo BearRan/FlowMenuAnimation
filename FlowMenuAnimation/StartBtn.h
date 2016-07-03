1ffa98abf248b2439b92d7f9d8d2579430115cf7
Bear <648070256@qq.com>
2016-07-03 19:02:36 +0800

    初步封装startbtn

---
+++ FlowMenuAnimation/StartBtn.h
@@ -0,0 +1,13 @@
+//
+//  StartBtn.h
+//  FlowMenuAnimation
+//
+//  Created by Bear on 16/7/3.
+//  Copyright © 2016年 Bear. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface StartBtn : UIButton
+
+@end
---
+++ FlowMenuAnimation/StartBtn.m
@@ -0,0 +1,13 @@
+//
+//  StartBtn.m
+//  FlowMenuAnimation
+//
+//  Created by Bear on 16/7/3.
+//  Copyright © 2016年 Bear. All rights reserved.
+//
+
+#import "StartBtn.h"
+
+@implementation StartBtn
+
+@end
